bcb9613f0d459dc069e13dfef17e0af6c915b281
mcrndz <jrmarcondesb@gmail.com>
2023-06-17 18:17:19 -0300

    ,,,,

--- lua/user/vscode/vscode-keymap.lua
+++ lua/user/vscode/vscode-keymap.lua
@@ -16,30 +16,6 @@ nnoremap <silent> n :call VSCodeNotify('editor.action.nextMatchFindAction')<CR>
 nnoremap <silent> N :call VSCodeNotify('editor.action.previousMatchFindAction')<CR>
 xnoremap <silent> N :call VSCodeNotify('editor.action.previousMatchFindAction')<CR>
 xnoremap <silent> n :call VSCodeNotify('editor.action.nextMatchFindAction')<CR>
-nnoremap <silent> <leader>t :call VSCodeNotify('workbench.action.togglePanel')<CR>
-nnoremap <silent> <leader>: :call VSCodeNotify('workbench.action.quickOpenView')<CR>
-xnoremap <silent> <leader>: :call VSCodeNotify('workbench.action.quickOpenView')<CR>
-nnoremap <silent> <leader>; :call VSCodeNotify('workbench.action.quickOpenView')<CR>
-xnoremap <silent> <leader>; :call VSCodeNotify('workbench.action.quickOpenView')<CR>
-nnoremap <silent> <leader>z :call VSCodeNotify('workbench.action.toggleZenMode')<CR>
+nnoremap <silent> <Space> :call VSCodeNotify('whichkey.show')<CR>
+xnoremap <silent> <Space> :call VSCodeNotify('whichkey.show')<CR>
 ]]
-
--- Vscode keybindings
---   // Neovim
---   { "key": "shift shift", "command": "workbench.action.showCommands"},
---   // Close find widget NVIM PURPOSE
---   { "key": "enter", "command": "closeFindWidget", "when": "findWidgetVisible && !isComposing" },
---   // Return to normal
---   { "command": "vscode-neovim.compositeEscape1", "key": "j", "when": "neovim.mode == insert && editorTextFocus", "args": "j"
---   }, // Disable crtl + movement commands  
---   { "command": "-vscode-neovim.send", "key": "ctrl+h", "when": "editorTextFocus", "args": "" },
---   { "command": "-vscode-neovim.send", "key": "ctrl+j", "when": "editortextfocus", "args": "" },
---   { "command": "-vscode-neovim.send", "key": "ctrl+k", "when": "editortextfocus", "args": "" },
---   { "command": "-vscode-neovim.send", "key": "ctrl+l", "when": "editorTextFocus", "args": "" },
---   /// Movement
---   {"key": "cmd+l", "command": "workbench.action.nextEditor" }, 
---   {"key": "cmd+h", "command": "workbench.action.previousEditor"},
---   {"key": "cmd+j", "command": "workbench.action.navigateDown"},
---   {"key": "cmd+d", "command": "vscode-neovim.ctrl-d"},
---   {"key": "cmd+u", "command": "vscode-neovim.ctrl-u"},
---   {"key": "cmd+i", "command": "workbench.action.focusActiveEditorGroup", "when": "!editorTextFocus" }
