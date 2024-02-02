9f07fd51b92f0b9a744bea5ca7353f31766a6633
mcrndz <jrmarcondesb@gmail.com>
2024-02-01 22:48:26 -0300

    finished fzf config

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,7 @@
+source $DOTFILES/fish/fisher.fish
+source $DOTFILES/fish/theme.fish
+source $DOTFILES/fzf/fzf.fish
+
 # Paths
 fish_add_path /opt/homebrew/sbin
 fish_add_path /opt/homebrew/bin
@@ -14,19 +18,13 @@ alias reload_fish="source $CONFIG_FISH"
 alias config_fish="$EDITOR $CONFIG_FISH"
 alias config_tmux="$EDITOR $CONFIG_TMUX"
 alias config_vim="$EDITOR $CONFIG_NVIM"
+alias j="jump"
 
 # Plugins
 # Add new plugins to the list by running `set plugins -a <author>/<plugin-name>`
-set -l plugins jorgebucaran/autopair.fish
-set -a plugins ilancosman/tide@v6
-
-# Auto install plugin if not installed
-for plugin in $plugins
-    if not fisher list | grep -q $plugin
-        echo $plugin >> $__fish_config_dir/fish_plugins
-        echo "run `fisher update` to complete the install of $plugin"
-    end
-end
+fisher_plugin_install jorgebucaran/autopair.fish
+fisher_plugin_install ilancosman/tide@v6
+fisher_plugin_install PatrickF1/fzf.fish
 
 # Autorun Tmux
 if status is-interactive
@@ -34,5 +32,6 @@ and not set -q TMUX
     exec tmux -f $DOTFILES/tmux/.tmux.conf
 end
 
-# Call Theme
-source $DOTFILES/fish/theme.fish
+jump shell fish | source
+
+# Configs
