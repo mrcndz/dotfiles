0b3adadbf085431b99c797f69fa1879a31873b14
mrcndz <jrmarcondesb@gmail.com>
2024-02-21 19:05:11 -0300

    added lsp to nvim config

---
+++ nvim/lua/plugin/lsp/mason.lua
@@ -0,0 +1,33 @@
+local mason = require("mason")
+local mason_lspconfig = require("mason-lspconfig")
+
+mason.setup({
+    ui = {
+        icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗"
+        }
+    }
+})
+
+mason_lspconfig.setup({
+    ensure_installed = {
+        "bashls",
+        "marksman",
+        "lua_ls",
+        "cmake",
+        "dockerls",
+        "cssls",
+        "dockerls",
+        "ltex",
+        "html",
+        "jsonls",
+        "pyright",
+        "rust_analyzer",
+        "tsserver",
+        "vimls",
+        "yamlls",
+    },
+    automatic_install = true,
+})
