42ee4875ca1fdd57072b4e8c75af2442bee4ad69
mcrndz <jrmarcondesb@gmail.com>
2024-02-21 19:05:11 -0300

    added lsp to nvim config

---
+++ nvim/lua/plugin/lsp/cmp.lua
@@ -0,0 +1,20 @@
+  -- Set up nvim-cmp.
+local cmp = require'cmp'
+
+cmp.setup({
+  sources = {
+    {name = 'nvim_lsp'},
+  },
+  mapping = cmp.mapping.preset.insert({
+    -- Enter key confirms completion item
+    ['<CR>'] = cmp.mapping.confirm({select = false}),
+
+    -- Ctrl + space triggers completion menu
+    ['<C-Space>'] = cmp.mapping.complete(),
+  }),
+  snippet = {
+    expand = function(args)
+      require('vsnip#anonymous')(args.body)
+    end,
+  },
+})
