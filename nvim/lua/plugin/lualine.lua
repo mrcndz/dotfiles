5b448628f2a6098034e4a5a1ff5581193aedac3f
mcrndz <jrmarcondesb@gmail.com>
2024-02-23 20:10:36 -0300

    changed to lualine

---
+++ nvim/lua/plugin/lualine.lua
@@ -0,0 +1,39 @@
+local function fileiconpath()
+  local filename = vim.fn.expand('%:t')
+  local extension = vim.fn.expand('%:e')
+  local filepath = vim.fn.expand('%:~:.')
+
+  local icon = require('nvim-web-devicons').get_icon(filename, extension)
+
+  if icon == nil then
+    return '' .. filepath
+  end
+
+  return icon .. ' ' .. filepath
+end
+
+require('lualine').setup {
+  options = {
+    icons_enabled = true,
+    theme = 'auto',
+    component_separators = { left = '', right = ''},
+    section_separators = { left = '', right = ''},
+    disabled_filetypes = { 'NvimTree' },
+  },
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = { 'branch', 'diagnostics'},
+    lualine_c = { fileiconpath,},
+    lualine_x = { 'diff' },
+    lualine_y = { 'location' },
+    lualine_z = { 'progress' }
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {'branch'},
+    lualine_c = {fileiconpath},
+    lualine_x = {},
+    lualine_y = {},
+    lualine_z = {}
+  },
+}
