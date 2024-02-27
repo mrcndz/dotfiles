23539742647f9696289c71da0996fa5baec52fb4
mrcndz <jrmarcondesb@gmail.com>
2024-02-27 13:00:54 -0300

    fixed lsp and added surround

--- nvim/lua/plugins/indent.lua
+++ nvim/lua/plugins/indent.lua
@@ -1,1 +1,15 @@
-{ "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} }
+return {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    opts = {
+    },
+    config = function()
+        local hooks = require "ibl.hooks"
+        require("ibl").setup {
+            whitespace = {
+                remove_blankline_trail = false,
+            },
+        }
+        hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
+    end
+}
--- nvim/lua/plugins/lsp/cmp.lua
+++ nvim/lua/plugins/lsp/cmp.lua
@@ -24,6 +24,7 @@ return {
 
             sources = cmp.config.sources({
                   { name = 'nvim_lsp' },
+                  { name = 'path' },
                   { name = 'luasnip' },
                 }, {
                   { name = 'buffer' },
