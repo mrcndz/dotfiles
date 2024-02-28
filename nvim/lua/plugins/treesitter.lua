fef218c666549ff1ba47328e231b00f2db390e51
mrcndz <jrmarcondesb@gmail.com>
2024-02-28 02:24:47 -0300

    added some plugins

---
+++ nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,49 @@
+return {
+	"nvim-treesitter/nvim-treesitter",
+
+	dependencies = {
+		"windwp/nvim-ts-autotag",
+	},
+
+	build = ":TSUpdate",
+	event = "bufWinEnter",
+
+	config = function()
+		local treesitter = require("nvim-treesitter.configs")
+		treesitter.setup({
+			ensure_installed = {
+				"bash",
+				"c",
+				"css",
+				"git_config",
+				"go",
+				"html",
+				"javascript",
+				"json",
+                "python",
+                "rust",
+				"jsdoc",
+                "fish",
+				"lua",
+				"sql",
+				"typescript",
+				"xml",
+				"yaml",
+				"markdown",
+				"markdown_inline",
+			},
+			sync_install = false,
+			auto_install = true,
+			highlight = {
+				enable = true,
+				additional_vim_regex_highlighting = false,
+			},
+            autotag = {
+                enable = true,
+            },
+            indent = {
+                enable = true,
+            },
+		})
+	end,
+}
