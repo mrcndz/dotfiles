1ca7fc0f2c14c366f05203c96d43fa2d0403f5ee
mcrndz <jrmarcondesb@gmail.com>
2024-02-28 19:35:40 -0300

    added theme and buffer keymap

--- nvim/lua/plugins/ui/themes.lua
+++ nvim/lua/plugins/ui/themes.lua
@@ -1,28 +1,59 @@
 return {
   {
-    "neanias/everforest-nvim",
-    version = false,
+    'rebelot/kanagawa.nvim',
     lazy = false,
-    config = function()
-      require("everforest").setup({})
-    end,
-  },
-  {
-    'ramojus/mellifluous.nvim',
-    -- version = "v0.*", -- uncomment for stable config (some features might be missed if/when v1 comes out)
     priority = 1000,
     config = function()
-        require'mellifluous'.setup({
-            comments = { italic = true },
-            transparent_background = {
-            enabled = true,
-            floating_windows = true,
-            telescope = true,
-            file_tree = true,
-            cursor_line = true,
-            status_line = false,
-            },
-        })
+      -- Default options:
+      require('kanagawa').setup({
+        compile = false, -- enable compiling the colorscheme
+        undercurl = true, -- enable undercurls
+        commentStyle = { italic = true },
+        functionStyle = {},
+        keywordStyle = { italic = true },
+        statementStyle = { bold = true },
+        typeStyle = {},
+        transparent = true,
+        dimInactive = false,
+        terminalColors = true,
+        colors = {
+          palette = {},
+          theme = { wave = {}, lotus = {}, dragon = {}, all = { ui = { bg_gutter = 'none' } } },
+        },
+        overrides = function(colors) -- add/modify highlights
+          local theme = colors.theme
+          return {
+            NormalFloat = { bg = 'none' },
+            FloatBorder = { bg = 'none' },
+            FloatTitle = { bg = 'none' },
+            NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
+            LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+            MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+            Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 },
+            PmenuSel = { fg = 'NONE', bg = theme.ui.bg_p2 },
+            PmenuSbar = { bg = theme.ui.bg_m1 },
+            PmenuThumb = { bg = theme.ui.bg_p2 },
+            TelescopeTitle = { fg = theme.ui.special, bold = true },
+            TelescopePromptNormal = { bg = theme.ui.bg_p1 },
+            TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = theme.ui.bg_p1 },
+            TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m1 },
+            TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = theme.ui.bg_m1 },
+            TelescopePreviewNormal = { bg = theme.ui.bg_dim },
+            TelescopePreviewBorder = { bg = theme.ui.bg_dim, fg = theme.ui.bg_dim },
+          }
+        end,
+
+        --  wave the default heart-warming theme,
+        --  dragon for those late-night sessions
+        --  lotus for when you're out in the open.
+        theme = 'dragon',
+        background = {
+          dark = 'dragon',
+          light = 'lotus',
+        },
+      })
+
+      vim.cmd('colorscheme kanagawa')
     end,
-  }
+  },
 }
