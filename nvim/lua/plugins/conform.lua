7415635a581edffe01f9fc90f9fbec657007a906
mcrndz <jrmarcondesb@gmail.com>
2024-02-28 02:24:47 -0300

    added some plugins

---
+++ nvim/lua/plugins/conform.lua
@@ -0,0 +1,24 @@
+return {
+  "stevearc/conform.nvim",
+  event = { "BufWritePre" },
+  cmd = { "ConformInfo" },
+  keys = {},
+  opts = {
+    formatters_by_ft = {
+      lua = { "stylua" },
+      python = { "black" },
+      javascript = { { "prettierd", "prettier" } },
+      json = { { "prettierd", "prettier" } },
+      fish = { "fish_indent" },
+    },
+    format_on_save = { timeout_ms = 500, lsp_fallback = true },
+    formatters = {
+      shfmt = {
+        prepend_args = { "-i", "2" },
+      },
+    },
+  },
+  init = function()
+    vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+  end,
+}
