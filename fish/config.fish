2a065d2e330cfa100843cbe993dd09a2d1a39ab8
mrcndz <jrmarcondesb@gmail.com>
2024-03-22 16:21:17 -0300

    fisher plugin updated

--- fish/config.fish
+++ fish/config.fish
@@ -3,7 +3,6 @@ set -gx EDITOR nvim
 set -gx DOTFILES $HOME/.dotfiles
 
 # Source
-source $DOTFILES/fish/fisher.fish
 source $DOTFILES/fish/theme.fish
 source $DOTFILES/fish/fzf.fish
 
@@ -13,19 +12,9 @@ fish_add_path /opt/homebrew/bin
 fish_add_path $HOME/.cargo/bin/
 
 # Alias
-alias reload_fish="source $CONFIG_FISH"
-alias config_fish="$EDITOR $CONFIG_FISH"
-alias config_tmux="$EDITOR $CONFIG_TMUX"
-alias config_vim="$EDITOR $CONFIG_NVIM"
 alias j="jump"
 alias v="nvim"
 alias g ="git"
-
-# Plugins
-# Add new plugins to the list by running `set plugins -a <author>/<plugin-name>`
-fisher_plugin_install jorgebucaran/autopair.fish
-fisher_plugin_install ilancosman/tide@v6
-fisher_plugin_install PatrickF1/fzf.fish
 
 # Autorun Tmux
 if status is-interactive
