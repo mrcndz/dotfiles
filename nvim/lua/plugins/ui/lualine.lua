70032029dacf5a2cc58d7f90a93c26510b2b0aef
mrcndz <jrmarcondesb@gmail.com>
2024-03-25 18:52:51 -0300

    fixes changed theme

--- nvim/lua/plugins/ui/lualine.lua
+++ nvim/lua/plugins/ui/lualine.lua
@@ -1,48 +1,63 @@
+local extra = {}
+extra.fancy_lsp_status = function()
+  local is_attached = vim.lsp.buf_is_attached
+  local icon = { on = ' ⚡', off = ' 💤' }
+
+  if is_attached then
+    return 'lsp' .. icon.on
+  end
+
+  return 'lsp' .. icon.off
+end
+
+extra.fancy_file_icon = function()
+  local filename = vim.fn.expand('%:t')
+  local extension = vim.fn.expand('%:e')
+  local filepath = vim.fn.expand('%:~:.')
+
+  local icon = require('nvim-web-devicons').get_icon(filename, extension)
+
+  if icon == nil then
+    return '' .. filepath
+  end
+
+  return icon .. ' ' .. filepath
+end
+
 return {
   {
     'nvim-lualine/lualine.nvim',
     lazy = false,
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
-      local function fileiconpath()
-        local filename = vim.fn.expand('%:t')
-        local extension = vim.fn.expand('%:e')
-        local filepath = vim.fn.expand('%:~:.')
+      local fileiconpath = extra.fancy_file_icon
+      local lspstatus = extra.fancy_lsp_status
 
-        local icon = require('nvim-web-devicons').get_icon(filename, extension)
-
-        if icon == nil then
-          return '' .. filepath
-        end
-
-        return icon .. ' ' .. filepath
-      end
-
-      require('lualine').setup {
+      require('lualine').setup({
         options = {
           icons_enabled = true,
           theme = 'auto',
-          component_separators = { left = '', right = ''},
-          section_separators = { left = '', right = ''},
+          component_separators = { left = '', right = '' },
+          section_separators = { left = '', right = '' },
           disabled_filetypes = { 'NvimTree' },
         },
         sections = {
-          lualine_a = {'mode'},
-          lualine_b = { 'branch', 'diagnostics'},
-          lualine_c = { fileiconpath,},
-          lualine_x = { 'diff' },
+          lualine_a = { 'mode' },
+          lualine_b = { 'branch', 'diagnostics' },
+          lualine_c = { fileiconpath },
+          lualine_x = { lspstatus },
           lualine_y = { 'location' },
-          lualine_z = { 'progress' }
+          lualine_z = { 'progress' },
         },
         inactive_sections = {
           lualine_a = {},
-          lualine_b = {'branch'},
-          lualine_c = {fileiconpath},
+          lualine_b = { 'branch' },
+          lualine_c = { fileiconpath },
           lualine_x = {},
           lualine_y = {},
-          lualine_z = {}
+          lualine_z = {},
         },
-      }
+      })
     end,
-  }
+  },
 }
--- nvim/lua/plugins/ui/themes.lua
+++ nvim/lua/plugins/ui/themes.lua
@@ -3,10 +3,13 @@ return {
     'marko-cerovac/material.nvim',
     lazy = false,
     priority = 1000,
+    init = function()
+      vim.g.material_style = 'deep ocean'
+    end,
     config = function()
       require('material').setup({
         disable = {
-          background = true,
+          background = false,
         },
       })
 
