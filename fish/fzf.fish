071402eb80370bd750e768968e5237624c72a5d5
Jose Marcondes Barros Junior-JMJUNIOR <jmjunior@motorola.com>
2024-07-09 10:40:33 -0300

    vi mode for fish wtf

--- fish/fzf.fish
+++ fish/fzf.fish
@@ -1,7 +1,21 @@
+function __fzf_preview__
+    if file --mime "$argv" | grep -q directory
+        tree -L 3 - "$argv"
+    else if file --mime "$argv" | grep -q binary
+        echo "Binary file: $argv"
+    else
+        if command --quiet --search bat
+            bat --style=numbers --color=always --line-range :250 "$argv"
+        else if command --quiet --search cat
+            cat "$argv" | head -n 250
+        else
+            head -n 250 "$argv"
+        end
+    end
+end
+
 # Default command
 fzf_configure_bindings --git_status=\eg --history=\ec --variables=\ev --processes=\ep --directory=\cf
-set fzf_dir_opts --bind "ctrl-e:execute(nvim {+1})"
-# set fzf_fd_opts '--ignore-file $DOTFILES/fzf/ignore'
 
 set -x FZF_DEFAULT_OPTS " \
     --multi \
@@ -17,4 +31,6 @@ set -x FZF_DEFAULT_OPTS " \
     --marker='✓ ' \
     --header='Search '
     --preview-window right:50%:wrap \
-    --ansi"
+    --ansi \
+    --preview='__fzf_preview__ {}' \
+    "
