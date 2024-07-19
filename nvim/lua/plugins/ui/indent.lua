105f38f30f79708f4eff707527968006fe07570f
mcrndz <jrmarcondesb@gmail.com>
2024-07-19 18:32:49 -0300

    added indentchunk and fixed whichkey cmd errors

--- nvim/lua/plugins/ui/indent.lua
+++ nvim/lua/plugins/ui/indent.lua
@@ -1,22 +1,61 @@
 return {
-  {
-    'lukas-reineke/indent-blankline.nvim',
-    main = 'ibl',
-    opts = {},
-    config = function()
-      local hooks = require('ibl.hooks')
-      require('ibl').setup({
-        whitespace = {
-          remove_blankline_trail = false,
-        },
-      })
-      hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
-    end,
-  },
+  -- Current using hlchnk :)
+  -- {
+  --   'lukas-reineke/indent-blankline.nvim',
+  --   main = 'ibl',
+  --   opts = {},
+  --   config = function()
+  --     local hooks = require('ibl.hooks')
+  --     require('ibl').setup({
+  --       whitespace = {
+  --         remove_blankline_trail = false,
+  --       },
+  --     })
+  --     hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
+  --   end,
+  -- },
   {
     'nmac427/guess-indent.nvim',
     config = function()
       require('guess-indent').setup({})
+    end,
+  },
+  {
+    'shellRaining/hlchunk.nvim',
+    event = { 'BufReadPre', 'BufNewFile' },
+    config = function()
+      require('hlchunk').setup({
+        chunk = {
+          enable = true,
+          chars = {
+            horizontal_line = '─',
+            vertical_line = '│',
+            left_top = '┌',
+            left_bottom = '└',
+            right_arrow = '─',
+          },
+          style = '#00ffff',
+          use_treesitter = true,
+        },
+        indent = {
+          enable = true,
+          use_treesitter = true,
+          chars = {
+            '│',
+            '¦',
+            '┆',
+            '┊',
+          },
+        },
+        line_num = {
+          enable = true,
+          style = '#00ffff',
+          use_treesitter = true,
+        },
+        blank = {
+          enable = true,
+        },
+      })
     end,
   },
 }
--- nvim/lua/plugins/which-key.lua
+++ nvim/lua/plugins/which-key.lua
@@ -38,7 +38,7 @@ return {
       icon = wkicon.get({ pattern = 'search', icon = ' ', color = 'green' }),
       { '<leader>f', find_file_and_update_nvim_tree, desc = 'Files', mode = 'n', icon = '' },
       { '<leader>F', '<cmd>Telescope live_grep theme=dropdown<cr>', desc = 'Grep', mode = 'n', icon = '' },
-      { '<leader>b', '<cmd>Telescope buffers<CR>', desc = 'Buffers', mode = 'n', icon = '' },
+      { '<leader><leader>', '<cmd>Telescope buffers<CR>', desc = 'Buffers', mode = 'n', icon = '' },
       { '<leader>c', '<cmd>Telescope commands<CR>', desc = 'Commands', mode = 'n', icon = '' },
       { '<leader>s', '<cmd>Telescope session-lens<CR>', desc = 'Session Lens', mode = 'n', icon = '' },
       { '<leader>h', '<cmd>Telescope oldfiles theme=dropdown<cr>', desc = 'Recents', mode = 'n', icon = '' },
