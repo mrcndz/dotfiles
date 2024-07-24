7fa987e652ae8a4950f3a77180459636be27a249
mrcndz <jrmarcondesb@gmail.com>
2024-07-24 18:54:04 -0300

    Matching substring too!

--- fish/scripts.fish
+++ fish/scripts.fish
@@ -10,6 +10,12 @@ function __cd_back_to
     if test -d $target
         cd $target
     else
-        echo "Directory $argv not found in path"
+        # Try matching a substring if the exact match fails
+        set target (string match -r ".*$argv.*" (pwd))
+        if test -d $target
+            cd $target
+        else
+            echo "Directory $argv not found in path"
+        end
     end
 end
