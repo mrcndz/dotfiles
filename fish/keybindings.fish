d39b055bf4533f46732e83d7f891d05725c22146
mcrndz <jrmarcondesb@gmail.com>
2024-07-24 19:48:04 -0300

    ok

--- fish/keybindings.fish
+++ fish/keybindings.fish
@@ -1,8 +1,8 @@
 # Enable Vi key bindings
 fish_vi_key_bindings
 
-bind -M insert \ce 'nvim (pwd)'
-bind -M default \ce 'nvim (pwd)'
+bind -M insert \ce nvim
+bind -M default \ce nvim
 bind -M insert \co "jump top | nvim (fzf)"
 bind -M default \co "jump top | nvim (fzf)"
 bind -M insert \cg lazygit
--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,7 @@
 vim.g.mapleader = ' '
 require('config.lazy')
+
+vim.cmd('colorscheme jellybeans-nvim')
 require('config.options')
 require('config.keymaps')
 require('config.commands')
-vim.g.mapleader = ' '
