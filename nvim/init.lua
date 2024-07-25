c4cd87f6e8fc454bc64a6f8d8a1ffb9e759ffcbf
mcrndz <jrmarcondesb@gmail.com>
2024-07-25 14:49:46 -0300

    updated for iguacu

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,7 +1,6 @@
 vim.g.mapleader = ' '
 require('config.lazy')
-
-vim.cmd('colorscheme jellybeans-nvim')
 require('config.options')
 require('config.keymaps')
 require('config.commands')
+vim.cmd('colorscheme jellybeans-nvim')
