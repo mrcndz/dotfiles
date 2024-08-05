fe3e81649e2fe970d0535e4d455c6ec03f70d2ed
Jose Marcondes Barros Junior-JMJUNIOR <jmjunior@motorola.com>
2024-08-05 12:03:06 -0300

    main

--- nvim/lua/plugins/diffview.lua
+++ nvim/lua/plugins/diffview.lua
@@ -1,4 +1,10 @@
 return {
   'sindrets/diffview.nvim',
   event = 'VeryLazy',
+  config = function()
+    default_args = {
+      DiffviewOpen = { "--untracked-files=no", "--imply-local" },
+      DiffviewFileHistory = { "--base=LOCAL" },
+    }
+  end,
 }
