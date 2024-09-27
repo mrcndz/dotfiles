7a7c4da76fe1a73a691de0b900c588f954520f5b
mrcndz <jrmarcondesb@gmail.com>
2024-09-27 13:25:43 -0300

    install

--- fish/plugins/fisher.fish
+++ fish/plugins/fisher.fish
@@ -1,3 +1,7 @@
 function install_fisher
-	curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
+    curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
+end
+
+function update_fisher
+    curl -sL https://git.io/fisher | source && fisher update
 end
