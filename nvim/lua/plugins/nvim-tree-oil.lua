3162a4aecf03520f178b812f9a32761e64fb9757
mrcndz <jrmarcondesb@gmail.com>
2024-10-26 22:41:21 -0300

    NTree changes

--- nvim/lua/plugins/nvim-tree-oil.lua
+++ nvim/lua/plugins/nvim-tree-oil.lua
@@ -3,6 +3,7 @@ return {
     'nvim-tree/nvim-tree.lua',
     version = '*',
     lazy = false,
+
     dependencies = {
       'nvim-telescope/telescope.nvim',
       'nvim-tree/nvim-web-devicons',
@@ -27,6 +28,7 @@ return {
           api.tree.find_file(filepath)
         end,
       })
+
       -- Autoclose
       vim.api.nvim_create_autocmd('QuitPre', {
         callback = function()
@@ -54,7 +56,7 @@ return {
       local function my_on_attach(bufnr)
         local oil = require('oil')
 
-        local function get_node_dirname()
+        local function edit_with_oil()
           local node = api.tree.get_node_under_cursor()
           local dirname = ''
 
@@ -70,19 +72,19 @@ return {
         end
 
         -- api.config.mappings.default_on_attach(bufnr)
-        vim.keymap.set('n', 'd', api.fs.remove, opts('Delete'))
         vim.keymap.set('n', 'y', api.fs.copy.filename, opts('Copy filename'))
         vim.keymap.set('n', 'Y', api.fs.copy.node, opts('Copy'))
         vim.keymap.set('n', 'r', api.fs.rename, opts('Rename'))
         vim.keymap.set('n', '?', api.tree.toggle_help, opts('Help'))
-        vim.keymap.set('n', '|', api.node.open.vertical, opts('Open: Vertical Split'))
-        vim.keymap.set('n', '-', api.node.open.horizontal, opts('Open: Horizontal Split'))
         vim.keymap.set('n', '>', api.tree.change_root_to_node, opts('CD'))
         vim.keymap.set('n', '<', api.tree.change_root_to_parent, opts('CD'))
+        vim.keymap.set('n', '|', api.node.open.vertical, opts('Open: Vertical Split'))
+        vim.keymap.set('n', '-', api.node.open.horizontal, opts('Open: Horizontal Split'))
         vim.keymap.set('n', 'l', api.node.open.edit, opts('Open'))
+        vim.keymap.set('n', 'o', api.node.open.edit, opts('Open'))
         vim.keymap.set('n', '<CR>', api.node.open.edit, opts('Open'))
-        vim.keymap.set('n', 't', api.tree.toggle_hidden_filter, opts('Toggle hidden'))
-        vim.keymap.set('n', 'i', get_node_dirname, opts('Edit'))
+        vim.keymap.set('n', '<C-p>', api.node.open.preview, opts('Preview'))
+        vim.keymap.set('n', 'i', edit_with_oil, opts('Edit'))
         vim.keymap.set('n', 'f', require('telescope.builtin').find_files, opts('Find directory'))
       end
 
@@ -113,6 +115,9 @@ return {
           enable = false,
           show_on_dirs = false,
         },
+        git = {
+          enable = true,
+        },
       })
     end,
   },
@@ -136,9 +141,9 @@ return {
           'icon',
         },
         keymaps = {
-          ['<CR>'] = 'actions.select',
           ['?'] = 'actions.show_help',
           ['q'] = quit_and_save,
+          ['<CR>'] = quit_and_save,
           ['<'] = 'actions.parent',
           ['gs'] = 'actions.change_sort',
           ['gx'] = 'actions.open_external',
