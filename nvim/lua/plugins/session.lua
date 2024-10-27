a698a7a829dfad278a55b0e2b72045446476b445
mrcndz <jrmarcondesb@gmail.com>
2024-10-26 22:40:45 -0300

    Refactoring auto-session

--- nvim/lua/plugins/session.lua
+++ nvim/lua/plugins/session.lua
@@ -1,35 +1,31 @@
-local function restore_nvim_tree()
-    local nvim_tree_api = require('nvim-tree.api')
-
-    nvim_tree_api.tree.search_node(vim.fn.getcwd())
-end
-
 return {
-    {
-        'rmagatti/auto-session',
-        dependencies = {
-            'nvim-telescope/telescope.nvim',
-        },
-        init = function()
-            vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
-        end,
-        config = function()
-            require('auto-session').setup({
-                auto_create = function()
-                    local cmd = 'git rev-parse --is-inside-work-tree'
-                    return vim.fn.system(cmd) == 'true\n'
-                end,
-                auto_save_enabled = true,
-                auto_restore_enabled = true,
-                log_level = 'error',
-                post_restore_cmds = { restore_nvim_tree },
-                auto_session_suppress_dirs = {
-                    '~/',
-                    '~/.temp/',
-                    '~/temp',
-                    '~/Downloads',
-                },
-            })
-        end,
+  {
+    'rmagatti/auto-session',
+    dependencies = {
+      'nvim-telescope/telescope.nvim',
     },
+    init = function()
+      vim.o.sessionoptions = 'blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions'
+    end,
+    config = function()
+      require('auto-session').setup({
+        auto_create = function()
+          local is_git_repo = vim.fn.system('git rev-parse --is-inside-work-tree') == 'true\n'
+          local cwd = vim.loop.cwd()
+          local is_top_level = vim.fn.system('git rev-parse --show-toplevel') == cwd .. '\n'
+
+          return (is_git_repo and is_top_level)
+        end,
+        auto_save_enabled = true,
+        auto_restore_enabled = true,
+        log_level = 'error',
+        auto_session_suppress_dirs = {
+          '~/',
+          '~/.temp/',
+          '~/temp',
+          '~/Downloads',
+        },
+      })
+    end,
+  },
 }
