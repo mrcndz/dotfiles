433c3ba5a3118352956c2be712821e1070968cd6
Jose Marcondes Barros Junior-JMJUNIOR <jmjunior@motorola.com>
2024-11-06 17:40:00 -0300

    style: format

--- nvim/lua/plugins/session.lua
+++ nvim/lua/plugins/session.lua
@@ -8,24 +8,32 @@ return {
       vim.o.sessionoptions = 'blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions'
     end,
     config = function()
-      require('auto-session').setup({
+      require('auto-session').setup {
         auto_create = function()
-          local is_git_repo = vim.fn.system('git rev-parse --is-inside-work-tree') == 'true\n'
+          local is_git_repo = vim.fn.system 'git rev-parse --is-inside-work-tree' == 'true\n'
           local cwd = vim.loop.cwd()
-          local is_top_level = vim.fn.system('git rev-parse --show-toplevel') == cwd .. '\n'
+          local is_top_level = vim.fn.system 'git rev-parse --show-toplevel' == cwd .. '\n'
 
           return (is_git_repo and is_top_level)
         end,
         auto_save_enabled = true,
         auto_restore_enabled = true,
         log_level = 'error',
+        post_restore_cmd = {
+          'DisableHLChunk',
+          'DisableHLIdent',
+          'DisableHLLineNum',
+          'EnableHLChunk',
+          'EnableHLIdent',
+          'EnableHLLineNum',
+        },
         auto_session_suppress_dirs = {
           '~/',
           '~/.temp/',
           '~/temp',
           '~/Downloads',
         },
-      })
+      }
     end,
   },
 }
