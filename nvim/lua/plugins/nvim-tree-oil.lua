f1018b5d5d556bebe10968b55db3635c6d8e97ac
Jose Marcondes Barros Junior-JMJUNIOR <jmjunior@motorola.com>
2024-11-06 17:39:45 -0300

    vim enter auto cd and set root

--- nvim/lua/plugins/nvim-tree-oil.lua
+++ nvim/lua/plugins/nvim-tree-oil.lua
@@ -17,15 +17,28 @@ return {
     end,
 
     config = function()
-      local ntree = require('nvim-tree')
-      local api = require('nvim-tree.api')
+      local ntree = require 'nvim-tree'
+      local api = require 'nvim-tree.api'
 
       -- Autocmds
       -- Auto focus file in nvim tree when buffer entered
       vim.api.nvim_create_autocmd({ 'BufEnter' }, {
         callback = function()
-          local filepath = vim.fn.expand('%:p')
+          local filepath = vim.fn.expand '%:p'
           api.tree.find_file(filepath)
+        end,
+      })
+
+      -- When opening nvim, open the tree in the git root
+      vim.api.nvim_create_autocmd({ 'VimEnter' }, {
+        callback = function()
+          local git_root = vim.fn.trim(vim.fn.system 'git rev-parse --show-toplevel')
+
+          if git_root ~= '' and not git_root:match '^fatal:' then
+            vim.api.nvim_cmd({ cmd = 'cd', args = { git_root } }, {})
+            api.tree.change_root(git_root)
+            api.tree.reload()
+          end
         end,
       })
 
@@ -37,7 +50,7 @@ return {
           local wins = vim.api.nvim_list_wins()
           for _, w in ipairs(wins) do
             local bufname = vim.api.nvim_buf_get_name(vim.api.nvim_win_get_buf(w))
-            if bufname:match('NvimTree_') ~= nil then
+            if bufname:match 'NvimTree_' ~= nil then
               table.insert(tree_wins, w)
             end
             if vim.api.nvim_win_get_config(w).relative ~= '' then
@@ -54,7 +67,7 @@ return {
       })
 
       local function my_on_attach(bufnr)
-        local oil = require('oil')
+        local oil = require 'oil'
 
         local function edit_with_oil()
           local node = api.tree.get_node_under_cursor()
@@ -72,23 +85,23 @@ return {
         end
 
         -- api.config.mappings.default_on_attach(bufnr)
-        vim.keymap.set('n', 'y', api.fs.copy.filename, opts('Copy filename'))
-        vim.keymap.set('n', 'Y', api.fs.copy.node, opts('Copy'))
-        vim.keymap.set('n', 'r', api.fs.rename, opts('Rename'))
-        vim.keymap.set('n', '?', api.tree.toggle_help, opts('Help'))
-        vim.keymap.set('n', '>', api.tree.change_root_to_node, opts('CD'))
-        vim.keymap.set('n', '<', api.tree.change_root_to_parent, opts('CD'))
-        vim.keymap.set('n', '|', api.node.open.vertical, opts('Open: Vertical Split'))
-        vim.keymap.set('n', '-', api.node.open.horizontal, opts('Open: Horizontal Split'))
-        vim.keymap.set('n', 'l', api.node.open.edit, opts('Open'))
-        vim.keymap.set('n', 'o', api.node.open.edit, opts('Open'))
-        vim.keymap.set('n', '<CR>', api.node.open.edit, opts('Open'))
-        vim.keymap.set('n', '<C-p>', api.node.open.preview, opts('Preview'))
-        vim.keymap.set('n', 'i', edit_with_oil, opts('Edit'))
-        vim.keymap.set('n', 'f', require('telescope.builtin').find_files, opts('Find directory'))
+        vim.keymap.set('n', 'y', api.fs.copy.filename, opts 'Copy filename')
+        vim.keymap.set('n', 'Y', api.fs.copy.node, opts 'Copy')
+        vim.keymap.set('n', 'r', api.fs.rename, opts 'Rename')
+        vim.keymap.set('n', '?', api.tree.toggle_help, opts 'Help')
+        vim.keymap.set('n', '>', api.tree.change_root_to_node, opts 'CD')
+        vim.keymap.set('n', '<', api.tree.change_root_to_parent, opts 'CD')
+        vim.keymap.set('n', '|', api.node.open.vertical, opts 'Open: Vertical Split')
+        vim.keymap.set('n', '-', api.node.open.horizontal, opts 'Open: Horizontal Split')
+        vim.keymap.set('n', 'l', api.node.open.edit, opts 'Open')
+        vim.keymap.set('n', 'o', api.node.open.edit, opts 'Open')
+        vim.keymap.set('n', '<CR>', api.node.open.edit, opts 'Open')
+        vim.keymap.set('n', '<C-p>', api.node.open.preview, opts 'Preview')
+        vim.keymap.set('n', 'i', edit_with_oil, opts 'Edit')
+        vim.keymap.set('n', 'f', require('telescope.builtin').find_files, opts 'Find directory')
       end
 
-      ntree.setup({
+      ntree.setup {
         on_attach = my_on_attach,
         actions = {
           open_file = {
@@ -118,24 +131,24 @@ return {
         git = {
           enable = true,
         },
-      })
+      }
     end,
   },
   {
     'stevearc/oil.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
-      local oil = require('oil')
+      local oil = require 'oil'
 
       local quit_and_save = function()
-        oil.save({
+        oil.save {
           confirm = true,
-        })
+        }
 
         oil.close()
       end
 
-      require('oil').setup({
+      require('oil').setup {
         default_file_explorer = false,
         columns = {
           'icon',
@@ -152,7 +165,7 @@ return {
           ['<C-p>'] = 'actions.preview',
         },
         use_default_keymaps = false,
-      })
+      }
     end,
   },
 }
