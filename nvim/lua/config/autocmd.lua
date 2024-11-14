8715ca5bfdab564d67aa18aa58e21ef652f45890
mcrndz <jrmarcondesb@gmail.com>
2024-11-14 01:17:13 -0300

    updated autocmd and options

--- nvim/lua/config/autocmd.lua
+++ nvim/lua/config/autocmd.lua
@@ -1,11 +1,1 @@
 local autocmd = vim.api.nvim_create_autocmd
-
-if package.loaded['nvim-tree'] then
-  autocmd({ 'BufEnter' }, {
-    callback = function()
-      local filepath = vim.fn.expand '%:p'
-      local ntree_api = require 'nvim-tree.api'
-      ntree_api.tree.find_file(filepath)
-    end,
-  })
-end
