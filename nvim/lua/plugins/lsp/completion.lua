55ff7e14045faa3011dce26a977abf84e57c029b
mcrndz <jrmarcondesb@gmail.com>
2024-11-14 03:12:28 -0300

    supermaven keymap integration with blink

--- nvim/lua/plugins/lsp/completion.lua
+++ nvim/lua/plugins/lsp/completion.lua
@@ -12,15 +12,23 @@ return {
         ['<Tab>'] = {
           function(cmp)
             local suggestion = require 'supermaven-nvim.completion_preview'
-            if suggestion.has_suggestion() then
-              return vim.schedule(suggestion.on_accept_suggestion)
+            local item = cmp.windows.autocomplete.get_selected_item()
+
+            if item == nil and suggestion.has_suggestion() then
+              vim.schedule(function()
+                -- LOL IT WORKED
+                vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<S-Tab>', true, false, true), 'i', true)
+              end)
+              return
+            elseif item == nil then
+              return
             end
+            vim.schedule(function()
+              cmp.windows.autocomplete.accept()
+            end)
+            return true
           end,
-          function(cmp)
-            if cmp.is_in_snippet() then
-              return cmp.accept()
-            end
-          end,
+          'snippet_forward',
           'fallback',
         },
         ['<C-Space>'] = { 'show', 'hide' },
@@ -53,7 +61,11 @@ return {
         },
         sources = {
           completion = {
-            enabled_providers = { 'lsp', 'path', 'buffer', 'snippets' },
+            enabled_providers = { 'lsp', 'path', 'snippets', 'buffer', 'lazydev' },
+          },
+          providers = {
+            lsp = { fallback_for = { 'lazydev' } },
+            lazydev = { name = 'LazyDev', module = 'lazydev.integrations.blink' },
           },
         },
       },
@@ -63,85 +75,12 @@ return {
     'supermaven-inc/supermaven-nvim',
     config = function()
       require('supermaven-nvim').setup {
-        disable_keymaps = true,
+        accept_suggestion = '<S-Tab>',
       }
-
-      local suggestion = require 'supermaven-nvim.completion_preview'
-      local accept_suggestion = function()
-        if suggestion.has_suggestion() then
-          suggestion.on_accept_suggestion()
-          return true
-        end
-        return false
-      end
     end,
   },
   {
     'hrsh7th/cmp-nvim-lsp',
     'hrsh7th/nvim-cmp',
   },
-  --   dependencies = {
-  --     'hrsh7th/cmp-buffer',
-  --     'hrsh7th/cmp-path',
-  --     'hrsh7th/cmp-cmdline',
-  --     'L3MON4D3/LuaSnip',
-  --     'saadparwaiz1/cmp_luasnip',
-  --     'hrsh7th/cmp-nvim-lsp-signature-help',
-  --     'onsails/lspkind-nvim',
-  --     'supermaven-inc/supermaven-nvim',
-  --   },
-  --   config = function()
-  --     --     local cmp = require 'cmp'
-  --     --     local lspkind = require 'lspkind'
-  --     --     local suggestion = require 'supermaven-nvim.completion_preview'
-  --     --     require('supermaven-nvim').setup { disable_keymaps = true }
-  --     --
-  --     --     cmp.setup {
-  --     --       snippet = {
-  --     --         expand = function(args)
-  --     --           require('luasnip').lsp_expand(args.body)
-  --     --         end,
-  --     --       },
-  --     --
-  --     --       sources = cmp.config.sources({
-  --     --         { name = 'nvim_lsp' },
-  --     --         { name = 'nvim_lsp_signature_help' },
-  --     --         { name = 'path' },
-  --     --         { name = 'luasnip' },
-  --     --       }, {
-  --     --         { name = 'buffer' },
-  --     --       }),
-  --     --
-  --     --       mapping = cmp.mapping.preset.insert {
-  --     --         ['<Tab>'] = cmp.mapping(function(fallback)
-  --     --           local entry = cmp.get_selected_entry()
-  --     --
-  --     --           if cmp.visible() and entry ~= nil then
-  --     --             cmp.confirm { select = false }
-  --     --           elseif suggestion.has_suggestion() then
-  --     --             suggestion.on_accept_suggestion()
-  --     --           else
-  --     --             fallback()
-  --     --           end
-  --     --         end, { 'i', 's' }),
-  --     --         ['<S-Tab>'] = cmp.mapping(function(fallback)
-  --     --           local entry = cmp.get_selected_entry()
-  --     --
-  --     --           if cmp.visible() and entry ~= nil then
-  --     --             cmp.mapping.close()
-  --     --           else
-  --     --             fallback()
-  --     --           end
-  --     --         end, { 'i', 's' }),
-  --     --         ['<CR>'] = cmp.mapping.confirm { select = false },
-  --     --         ['<C-j>'] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert },
-  --     --         ['<C-k>'] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert },
-  --     --         ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-  --     --         ['<C-u>'] = cmp.mapping.scroll_docs(4),
-  --     --         ['<C-Enter>'] = cmp.mapping.complete(),
-  --     --         ['<C-Space>'] = cmp.mapping.complete(),
-  --     --       },
-  --     --     }
-  --   end,
-  -- },
 }
