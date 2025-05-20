57d3d5bb9ed831aa2df5ea686fa4e2e0e1b94e6e
mcrndz <jrmarcondesb@gmail.com>
2025-05-20 15:17:11 -0300

    removing some things

--- nvim/lsp/typescript.lua
+++
@@ -1,8 +0,0 @@
-return {
-  -- cmd = { 'typescript-language-server', '--stdio' },
-  -- filetypes = { 'typescript', 'javascript' },
-  -- root_markers = { '.git', '.eslintrc.js', '.prettierrc', '.prettierrc.js' },
-  -- settings = {
-  --   capabilites = require('blink.cmp').get_lsp_capabilities(vim.lsp.protocol.make_client_capabilities()),
-  -- },
-}
