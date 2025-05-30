52e4f78859fc9de4eb09f9056337d51706d34bf1
mrcndz <jrmarcondesb@gmail.com>
2025-05-30 20:49:40 -0300

    fix(lint): pylin not using pyproject.toml

--- nvim/lua/plugins/lsp/lint.lua
+++ nvim/lua/plugins/lsp/lint.lua
@@ -13,7 +13,14 @@ return {
         javascript = { 'eslint' },
       }
 
+      local pylint = lint.linters.pylint
+      pylint.args {
+        '--rcfile',
+        vim.fs.find('pyproject.toml')[1],
+      }
+
       local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
+
       vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
         group = lint_augroup,
         callback = function()
