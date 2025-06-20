f9fbe18c0f8a591e71a5868cf70fd66fbd3325b6
mrcndz <jrmarcondesb@gmail.com>
2025-06-19 22:27:05 -0300

    refactor(keymaps): replace keymap definitions with vim.keymap.set

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,29 +1,19 @@
-local Utils = require 'utils'
-local noremap = Utils.noremap
-local nnoremap = Utils.nnoremap
-local vnoremap = Utils.vnoremap
-local inoremap = Utils.inoremap
-local nmap = Utils.nmap
+vim.keymap.set('n', '<space>', '<Nop>', { silent = true, desc = 'Disable space key' })
+vim.keymap.set('n', ';', ':', { silent = true, desc = 'Enter command mode' })
+vim.keymap.set('n', 'q;', 'q:', { silent = true, desc = 'Open command-line window' })
 
-nmap('<space>', '<Nop>')
-nmap(';', ':')
-nmap('q;', 'q:')
+vim.keymap.set('i', 'jj', '<Esc>', { noremap = true, silent = true, desc = 'Quick escape from insert mode' })
+vim.keymap.set('i', 'jk', '<Esc>', { noremap = true, silent = true, desc = 'Quick escape from insert mode' })
+vim.keymap.set('i', '<Esc>', '<Esc>', { noremap = true, silent = true, desc = 'Escape from insert mode' })
 
--- map leader to space
-inoremap('jj', '<Esc>')
-inoremap('<Esc>', '<Esc>')
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv", { noremap = true, silent = true, desc = 'Move selected text down' })
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv", { noremap = true, silent = true, desc = 'Move selected text up' })
 
--- Move text
-vnoremap('J', ":m '>+1<CR>gv=gv")
-vnoremap('K', ":m '<-2<CR>gv=gv")
+vim.keymap.set('n', 'n', 'nzzzv', { noremap = true, silent = true, desc = 'Next search result (centered)' })
+vim.keymap.set('n', 'N', 'Nzzzv', { noremap = true, silent = true, desc = 'Previous search result (centered)' })
 
--- search centering
-nnoremap('n', 'nzzzv')
-nnoremap('N', 'Nzzzv')
-
--- Buffer
-nnoremap('<Tab>', ':bprevious<CR>')
-nnoremap('<S-Tab>', ':bnext<CR>')
+vim.keymap.set('n', '<Tab>', ':bprevious<CR>', { noremap = true, silent = true, desc = 'Previous buffer' })
+vim.keymap.set('n', '<S-Tab>', ':bnext<CR>', { noremap = true, silent = true, desc = 'Next buffer' })
 
 -- Macro over visual
 vim.cmd [[
@@ -36,28 +26,25 @@ vim.cmd [[
 
 ]]
 
-nnoremap('<C-s>', ':w<CR>')
-nnoremap('<C-q>', ':q<CR>')
+vim.keymap.set('n', '<C-s>', ':w<CR>', { noremap = true, silent = true, desc = 'Save file' })
+vim.keymap.set('n', '<C-q>', ':q<CR>', { noremap = true, silent = true, desc = 'Quit' })
 
--- Yank to end of line
-nnoremap('Y', 'y$')
+vim.keymap.set('n', 'Y', 'y$', { noremap = true, silent = true, desc = 'Yank to end of line' })
 
 -- Different register for delete and paste
--- nnoremap('<leader>d', '"_d')
--- vnoremap('<leader>d', '"_d')
-nnoremap('d', '"_d')
-vnoremap('d', '"_d')
-nnoremap('x', '"_x')
-vnoremap('p', '"_dP')
-nnoremap('P', '"_d')
-vnoremap('<LeftRelease>', '"+y<LeftRelease>')
+vim.keymap.set('n', 'd', '"_d', { noremap = true, silent = true, desc = 'Delete to black hole register' })
+vim.keymap.set('v', 'd', '"_d', { noremap = true, silent = true, desc = 'Delete to black hole register' })
+vim.keymap.set('n', 'x', '"_x', { noremap = true, silent = true, desc = 'Delete character to black hole register' })
+vim.keymap.set('v', 'p', '"_dP', { noremap = true, silent = true, desc = 'Paste over selection (keep original)' })
+vim.keymap.set('n', 'P', '"_d', { noremap = true, silent = true, desc = 'Delete to black hole register' })
+vim.keymap.set('v', '<LeftRelease>', '"+y<LeftRelease>', { noremap = true, silent = true, desc = 'Copy selection to clipboard on mouse release' })
 
 -- Copy to system clippboard
-nnoremap('y', '"+y')
-vnoremap('y', '"+y')
-vnoremap('<C-c>', '"+y') -- Windows behavior
+vim.keymap.set('n', 'y', '"+y', { noremap = true, silent = true, desc = 'Yank to system clipboard' })
+vim.keymap.set('v', 'y', '"+y', { noremap = true, silent = true, desc = 'Yank to system clipboard' })
+vim.keymap.set('v', '<C-c>', '"+y', { noremap = true, silent = true, desc = 'Copy to system clipboard (Windows behavior)' })
 
 -- Paste from system clippboard
-nnoremap('p', '"+p')
-vnoremap('p', '"+p')
-vnoremap('<C-v>', '"+p') -- Windows behavior
+vim.keymap.set('n', 'p', '"+p', { noremap = true, silent = true, desc = 'Paste from system clipboard' })
+vim.keymap.set('v', 'p', '"+p', { noremap = true, silent = true, desc = 'Paste from system clipboard' })
+vim.keymap.set('v', '<C-v>', '"+p', { noremap = true, silent = true, desc = 'Paste from system clipboard (Windows behavior)' })
