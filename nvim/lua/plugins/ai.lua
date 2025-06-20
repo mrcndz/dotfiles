c94c77cb976ad67100f45899f2711ab56f055d4b
mrcndz <jrmarcondesb@gmail.com>
2025-06-19 22:27:05 -0300

    refactor(keymaps): replace keymap definitions with vim.keymap.set

--- nvim/lua/plugins/ai.lua
+++ nvim/lua/plugins/ai.lua
@@ -1,11 +1,27 @@
 return {
   'coder/claudecode.nvim',
-  config = true,
-  opts = {
-    terminal = {
-      provider = 'snacks',
-    },
-  },
+  config = function()
+    require('claudecode').setup {
+      terminal = {
+        provider = 'snacks',
+      },
+    }
+
+    vim.api.nvim_create_autocmd('TermOpen', {
+      group = vim.api.nvim_create_augroup('ClaudeTermMapping', { clear = true }),
+      pattern = 'term://*:claude*',
+      callback = function()
+        local opts = { silent = true, noremap = true, buffer = 0 }
+
+        vim.keymap.set('t', 'jk', '<C-\\><C-n>', opts)
+        vim.keymap.set('t', 'jj', '<C-\\><C-n>', opts)
+        vim.keymap.set('t', '<C-h>', '<cmd>NvimTmuxNavigateLeft<cr>', opts)
+        vim.keymap.set('t', '<C-l>', '<cmd>NvimTmuxNavigateRight<cr>', opts)
+      end,
+    })
+
+    return true
+  end,
   keys = {
     { '<leader>ac', '<cmd>ClaudeCode<cr>', desc = 'Toggle Claude' },
     { '<leader>af', '<cmd>ClaudeCodeFocus<cr>', desc = 'Focus Claude' },
