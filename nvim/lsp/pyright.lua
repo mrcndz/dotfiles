17cb34fde5fded5a336ebd88ac47980b329e6eed
mrcndz <jrmarcondesb@gmail.com>
2025-07-03 21:00:55 -0300

    fix(pyright): set dynamicRegistration to false

--- nvim/lsp/pyright.lua
+++ nvim/lsp/pyright.lua
@@ -3,7 +3,12 @@ return {
   filetypes = { 'python' },
   root_markers = { '.venv', '.env', 'pyproject.toml', 'setup.py', 'requirements.txt', 'Pipfile', 'poetry.lock' },
   settings = {
-    capabilities = require('blink.cmp').get_lsp_capabilities(vim.lsp.protocol.make_client_capabilities()),
+    capabilities = function()
+      local c = require('blink.cmp').get_lsp_capabilities(vim.lsp.protocol.make_client_capabilities())
+      c.workspace.didChangeWatchedFiles.dynamicRegistration = false
+
+      return c
+    end,
     python = {
       analysis = {
         autoSearchPaths = true,
